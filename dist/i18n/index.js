26fd1b3422f7f1ff0043754461c944f02190078a
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-07-22 10:54:34 +0300

    Add files via upload

---
+++ dist/i18n/index.js
@@ -0,0 +1,29 @@
+import ru from './ru.js';
+import en from './en.js';
+import de from './de.js';
+import fr from './fr.js';
+import nl from './nl.js';
+import pl from './pl.js';
+import sv from './sv.js';
+import hu from './hu.js';
+import cs from './cs.js';
+import it from './it.js';
+import pt from './pt.js';
+import sl from './sl.js';
+
+export const SWITCH_TRANSLATIONS = {
+  ru,
+  en,
+  de,
+  fr,
+  nl,
+  pl,
+  sv,
+  hu,
+  cs,
+  it,
+  pt,
+  sl,
+};
+
+export default SWITCH_TRANSLATIONS;
